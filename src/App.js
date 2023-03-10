3605e486e98ee177afba795c468e4c844b67294b
ThurainTun <stp7112004@gmail.com>
2023-03-11 01:42:56 +0630

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import QAImage from "./assets/image.jpg";
 
 function App() {
+  let name = "Hlaing Min Than";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {name}</h1>
+
+      {/* 1st way -  public folder way */}
+
+      {/* absolute path */}
+      {/* <img src="http://localhost:3000/image.jpg" alt="" /> */}
+      {/* relative path */}
+      {/* <img src="/image.jpg" alt="" /> */}
+
+      {/* 2nd import */}
+      <img src={QAImage} alt="" />
     </div>
   );
 }
